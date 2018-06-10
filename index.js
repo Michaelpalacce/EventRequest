3a2c649d7b67a8808fa20b798d7126ce9e29ba36
Michaelpalacce <stefantigro@gmail.com>
2018-06-10 19:43:18 +0300

    Now able to set protocol to be used

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // Dependencies
 const http					= require( 'http' );
+const https					= require( 'https' );
 const middlewaresContainer	= require( './server/middleware_container' );
 const Router				= require( './server/router' );
 const RequestEvent			= require( './server/event' );
@@ -11,14 +12,65 @@ const BodyParserHandler		= require( './server/middlewares/body_parser_handler' )
 const Logger				= require( './server/middlewares/logger' );
 
 /**
+ * @brief	Constants
+ */
+const PROTOCOL_HTTP							= 'http';
+const PROTOCOL_HTTPS						= 'https';
+const OPTIONS_PARAM_PORT					= 'port';
+const OPTIONS_PARAM_PORT_DEFAULT			= 3000;
+const OPTIONS_PARAM_PROTOCOL				= 'protocol';
+const OPTIONS_PARAM_PROTOCOL_DEFAULT		= PROTOCOL_HTTP;
+const OPTIONS_PARAM_HTTPS					= 'httpsOptions';
+const OPTIONS_PARAM_HTTPS_DEFAULT			= {};
+
+const POSSIBLE_PROTOCOL_OPTIONS				= {};
+POSSIBLE_PROTOCOL_OPTIONS[PROTOCOL_HTTP]	= http;
+POSSIBLE_PROTOCOL_OPTIONS[PROTOCOL_HTTPS]	= https;
+
+/**
  * @brief	Server class responsible for receiving requests and sending responses
  */
 class Index
 {
-	constructor()
+	/**
+	 * @brief	Passes options for server configuration
+	 *
+	 * @param	Object options
+	 */
+	constructor( options	= {} )
 	{
-		this.server	= {};
-		this.router	= new Router();
+		this.options	= options;
+
+		this.server		= {};
+		this.router		= new Router();
+
+		this.sanitizeConfig();
+	}
+
+	/**
+	 * @brief	Sets defaults for the server options
+	 *
+	 * @return	void
+	 */
+	sanitizeConfig()
+	{
+		let options								= this.options;
+
+		let protocol							= options[OPTIONS_PARAM_PROTOCOL];
+		this.options[OPTIONS_PARAM_PROTOCOL]	= typeof protocol === 'string'
+												&& typeof POSSIBLE_PROTOCOL_OPTIONS[protocol] !== 'undefined'
+												? protocol
+												: OPTIONS_PARAM_PROTOCOL_DEFAULT;
+
+		let httpsOptions						= this.options[OPTIONS_PARAM_HTTPS];
+		this.options[OPTIONS_PARAM_HTTPS]		= typeof httpsOptions === 'object'
+												? httpsOptions
+												: OPTIONS_PARAM_HTTPS_DEFAULT;
+
+		let port								= options[OPTIONS_PARAM_PORT];
+		this.options[OPTIONS_PARAM_PORT]		= typeof port === 'number'
+												? port
+												: OPTIONS_PARAM_PORT_DEFAULT;
 	}
 
 	/**
@@ -68,10 +120,9 @@ class Index
 	 *
 	 * @return	void
 	 */
-	start ( port )
+	start ()
 	{
-		// Create the server
-		this.server			= http.createServer( ( req, res ) => {
+		let unifiedServerCallback	= ( req, res ) =>{
 			let requestEvent	= Index.resolve( req, res );
 
 			res.on( 'finish', () => {
@@ -87,7 +138,7 @@ class Index
 
 			try
 			{
-				let block	= this.	router.getExecutionBlockForCurrentEvent( requestEvent );
+				let block	= this.router.getExecutionBlockForCurrentEvent( requestEvent );
 				requestEvent.setBlock( block );
 				requestEvent.next();
 			}
@@ -95,11 +146,17 @@ class Index
 			{
 				requestEvent.sendError( e );
 			}
-		});
+		};
 
-		this.server.listen( port, () =>
+		// Create the server
+		let protocol		= this.options[OPTIONS_PARAM_PROTOCOL];
+		this.server			= protocol === PROTOCOL_HTTPS
+							? https.createServer( this.options[OPTIONS_PARAM_HTTPS], unifiedServerCallback )
+							: http.createServer( unifiedServerCallback );
+
+		this.server.listen( this.options[OPTIONS_PARAM_PORT], () =>
 			{
-				console.log( 'Server successfully started and listening on port', port );
+				console.log( 'Server successfully started and listening on port', this.options[OPTIONS_PARAM_PORT] );
 			}
 		);
 
