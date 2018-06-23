470a31ccb87e6b30b29bd1c77db7ba8a2f64a36a
Michaelpalacce <stefantigro@gmail.com>
2018-06-23 22:40:11 +0300

    Mocker implemented

--- server/components/tester/mocker.js
+++ server/components/tester/mocker.js
@@ -3,6 +3,154 @@
 const assert	= require( 'assert' );
 
 /**
+ * @brief	Method mock. This class is responsible for mocking a method and attaching it to the mocked class
+ */
+class MethodMock
+{
+	constructor( mockedClass, mockMethodOptions )
+	{
+		this.mockedClass		= mockedClass;
+		this.mockedMethod		= ()=>{};
+
+		this.method				= typeof mockMethodOptions.method === 'string'
+								? mockMethodOptions.method
+								: null;
+
+		this.withArguments		= Array.isArray( mockMethodOptions.with )
+								? mockMethodOptions.with
+								: [];
+
+		this.executionTimes		= typeof mockMethodOptions.called === 'number'
+								? mockMethodOptions.called
+								: null;
+
+		let shouldReturn		= typeof mockMethodOptions.shouldReturn !== 'undefined'
+								? mockMethodOptions.shouldReturn
+								: null;
+
+		let onConsecutiveCalls	= Array.isArray( mockMethodOptions.onConsecutiveCalls )
+								? mockMethodOptions.onConsecutiveCalls
+								: null;
+
+		if (
+			this.mockedClass === undefined
+			|| this.method === null
+			|| ( shouldReturn === null && onConsecutiveCalls === null )
+		) {
+			throw new Error( 'Invalid mock options provided' );
+		}
+
+		this.executionBlock	= shouldReturn === null ? onConsecutiveCalls : [shouldReturn];
+
+		this.setUpMockedMethod();
+	}
+
+	/**
+	 * @brief	Creates the mocked method that should later be attached to the mocked class
+	 *
+	 * @return	void
+	 */
+	setUpMockedMethod()
+	{
+		let functionCalled	= 0;
+
+		this.mockedMethod	= ( ...args ) => {
+			functionCalled ++;
+
+			if ( this.executionTimes !== null && this.executionTimes < functionCalled )
+			{
+				throw new Error( `Method ${this.method} was not expected to be called more than ${this.executionTimes} times.` );
+			}
+
+			this.assertWithArguments( args );
+
+			return this.getNextExecutionFunction( args );
+		};
+	}
+
+	/**
+	 * @brief	Assert whether correct with arguments are passed
+	 *
+	 * @return	void
+	 */
+	assertWithArguments( args )
+	{
+		if ( this.hasWithArguments() )
+		{
+			let currentArguments	= this.getWithArguments();
+			if ( ! Array.isArray( currentArguments ) )
+			{
+				throw new Error( 'Invalid arguments provided' );
+			}
+			for ( let index = 0; index < currentArguments.length; ++ index )
+			{
+				let value	= currentArguments[index];
+				assert.strictEqual( value, args[index], `Failed asserting that ${this.method} was called `
+					+ ` with correct argument at position ${index}. `
+					+ `${value} was expected, but ${args[index]} received`
+				)
+			}
+		}
+	}
+
+	/**
+	 * @brief	Attaches the mocked method to the mocked class
+	 *
+	 * @return	void
+	 */
+	attachMockedMethod()
+	{
+		if ( typeof this.mockedClass[this.method] !== 'function' )
+		{
+			throw new Error( 'Trying to mock a method that does not exist.' );
+		}
+
+		this.mockedClass[this.method]	= this.mockedMethod;
+	}
+
+	/**
+	 * @brief	Checks to see if there are with arguments
+	 *
+	 * @return	Boolean
+	 */
+	hasWithArguments()
+	{
+		return this.withArguments.length > 0;
+	}
+
+	/**
+	 * @brief	Gets the next with arguments
+	 *
+	 * @return	Array
+	 */
+	getWithArguments()
+	{
+		return this.withArguments.length === 1 ? this.withArguments[0] : this.withArguments.shift();
+	}
+
+	/**
+	 * @brief	Gets the next function to execute
+	 *
+	 * @param	Array args
+	 *
+	 * @return	mixed
+	 */
+	getNextExecutionFunction( args )
+	{
+		let functionToExecute	= this.executionBlock.length > 1 ? this.executionBlock.shift() : this.executionBlock[0];
+
+		if ( typeof functionToExecute === 'function' )
+		{
+			return functionToExecute.apply( this.mockedClass, args )
+		}
+		else
+		{
+			return functionToExecute;
+		}
+	}
+}
+
+/**
  * @brief	Used to create a MockedObject
  *
  * @param	Object objectToMock
@@ -22,75 +170,8 @@ let Mock	= function ( objectToMock )
 		 */
 		_mock( mockMethodOptions )
 		{
-			let method				= typeof mockMethodOptions.method === 'string'
-									? mockMethodOptions.method
-									: null;
-
-			let shouldReturn		= typeof mockMethodOptions.shouldReturn !== 'undefined'
-									? mockMethodOptions.shouldReturn
-									: null;
-
-			let withArguments		= Array.isArray( mockMethodOptions.with )
-									? mockMethodOptions.with
-									: [];
-
-			let called				= typeof mockMethodOptions.called === 'number'
-									? mockMethodOptions.called
-									: null;
-
-			let onConsecutiveCalls	= Array.isArray( mockMethodOptions.onConsecutiveCalls )
-									? mockMethodOptions.onConsecutiveCalls
-									: null;
-
-			if ( method === null || ( shouldReturn === null && onConsecutiveCalls === null ) )
-			{
-				throw new Error( 'Invalid mock options provided' );
-			}
-
-			onConsecutiveCalls	= shouldReturn === null ? onConsecutiveCalls : [shouldReturn];
-
-			if ( typeof this[method] !== 'function' )
-			{
-				throw new Error( 'Trying to mock a method that does not exist.' );
-			}
-
-			let functionCalled	= 0;
-
-			this[method]		= ( ...args ) => {
-				functionCalled ++;
-				if ( called !== null && called < functionCalled )
-				{
-					throw new Error( `Method ${method} was not expected to be called more than ${called} times.` );
-				}
-
-				if ( withArguments.length > 0 )
-				{
-					let currentArguments	= withArguments.length === 1 ? withArguments[0] : withArguments.shift();
-					if ( ! Array.isArray( currentArguments ) )
-					{
-						throw new Error( 'Invalid arguments provided' );
-					}
-					for ( let index = 0; index < currentArguments.length; ++ index )
-					{
-						let value	= currentArguments[index];
-						assert.strictEqual( value, args[index], `Failed asserting that ${method} was called `
-							+ ` with correct argument at position ${index}. `
-							+ `${value} was expected, but ${args[index]} received`
-						)
-					}
-				}
-
-				let functionToExecute	= onConsecutiveCalls.length > 1 ? onConsecutiveCalls.shift() : onConsecutiveCalls[0];
-
-				if ( typeof functionToExecute === 'function' )
-				{
-					return functionToExecute.call( this, args )
-				}
-				else
-				{
-					return functionToExecute;
-				}
-			};
+			let methodMock	= new MethodMock( this, mockMethodOptions );
+			methodMock.attachMockedMethod();
 
 			return this;
 		}
@@ -99,22 +180,4 @@ let Mock	= function ( objectToMock )
 	return MockedClass;
 };
 
-class Test
-{
-	methodToMock() { return 'hey'; }
-}
-
-let MockedTest	= Mock( Test );
-let mockedTest	= new MockedTest();
-mockedTest._mock({
-	method				: 'methodToMock',
-	onConsecutiveCalls	: ['MOCKED', ()=>{ return 'MOCKED AGAIN'; }],
-	with				: [
-		['hello', 'test'],
-		['secondHello', 'secondTest']
-	],
-	called				: 2
-});
-
-console.log( mockedTest.methodToMock( 'hello', 'test' ) );
-console.log( mockedTest.methodToMock( 'secondHello', 'secondTest' ) );
+module.exports	= Mock;
