b0c1cf883cd9b57f6722769d346d371574c4095b
Stefan Genov <michaelpalacce@gmail.com>
2019-01-19 15:20:18 +0200

    [master] Added tests

--- tests/server/plugins/available_plugins/env_plugin_test.js
+++ tests/server/plugins/available_plugins/env_plugin_test.js
@@ -5,20 +5,34 @@ const { Mock, assert, test, helpers }	= require( '../../../test_helper' );
 const EnvPlugin							= require( '../../../../server/plugins/available_plugins/env_plugin' );
 const Server							= require( '../../../../server/server' );
 const path								= require( 'path' );
+const fs								= require( 'fs' );
 
 test({
 	message	: 'EnvPlugin setServerOnRuntime',
 	test	: ( done )=>{
-		let MockServer	= Mock( Server );
-		let server		= new MockServer();
+		let MockServer		= Mock( Server );
+		let server			= new MockServer();
+		let fileLocation	= path.join( __dirname, '/fixture/.env');
 
-		let envPlugin	= new EnvPlugin( 'id', {
-			fileLocation	: path.join( __dirname, '/fixture/.env'),
+		let envPlugin		= new EnvPlugin( 'id', {
+			fileLocation,
 			callback		: ( error )=>{
 				assert.equal( false, error );
 				assert.equal( 'TESTVALUE', process.env.TESTKEY );
 				assert.equal( 'TESTVALUE=WITH=ENTER', process.env.TESTKEYTWO );
-				done();
+
+				let writeStream	= fs.createWriteStream( fileLocation );
+
+				writeStream.write( 'TEST=VALUE' );
+				writeStream.end();
+
+				// Wait for the file system watcher to find the changes
+				setTimeout(()=>{
+					assert.equal( 'VALUE', process.env.TEST );
+					assert.equal( true, typeof process.env.TESTKEY === 'undefined' );
+					assert.equal( true, typeof process.env.TESTKEYTWO === 'undefined' );
+					done();
+				}, 250 );
 			}
 		} );
 
