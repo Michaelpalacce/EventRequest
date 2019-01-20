8146165d70d6aacdc6297dbc03af7226d417ebf2
michaelpalacce <michaelpalacce@gmail.com>
2019-01-20 17:49:16 +0200

    [master] Added documentation

--- server/components/caching/data_server_model_creator.js
+++ server/components/caching/data_server_model_creator.js
@@ -9,13 +9,18 @@
  *
  * @return	Object
  */
-module.exports	= function ( dataServer, namespace, options = {} )
+module.exports	= function ( dataServer, namespace, validationSchema = {} )
 {
 	/**
 	 * @brief	Data server model that is attached to a specific namespace and can easily create and save data there.
 	 */
 	class ModelClass
 	{
+		/**
+		 * @param	String recordName
+		 * @param	Object recordData
+		 * @param	Options recordOptions
+		 */
 		constructor( recordName, recordData = {}, recordOptions = {} )
 		{
 			this.recordName		= recordName;
@@ -23,6 +28,15 @@ module.exports	= function ( dataServer, namespace, options = {} )
 			this.recordOptions	= recordOptions;
 		}
 
+		/**
+		 * @brief	Saves the data of the current Model
+		 *
+		 * @details	This will save the current state of the object and will attempt to save that.
+		 * 			This is done so you can change it immediately after and save the new model
+		 * 			This will not create the namespace if it does not exist
+		 *
+		 * @return	Promise
+		 */
 		save()
 		{
 			let recordName		= this.recordName;
@@ -36,6 +50,36 @@ module.exports	= function ( dataServer, namespace, options = {} )
 			});
 		}
 
+		/**
+		 * @brief	Removes the namespace if it exists
+		 *
+		 * @return	Promise
+		 */
+		static removeNamespaceIfExists()
+		{
+			return new Promise(( resolve, reject )=>{
+				dataServer.existsNamespace( namespace ).then(( exists )=>{
+					if ( ! exists )
+					{
+						dataServer.removeNamespace( namespace ).then(()=>{
+							resolve( false );
+						}).catch( reject );
+					}
+					else
+					{
+						resolve( false );
+					}
+				}).catch( reject );
+			});
+		}
+
+		/**
+		 * @brief	Attempts to create the model's namespace if it does not exist
+		 *
+		 * @details	If the namespace exists, do nothing
+		 *
+		 * @return	Promise
+		 */
 		static createNamespaceIfNotExists()
 		{
 			return new Promise(( resolve, reject )=>{
@@ -54,6 +98,13 @@ module.exports	= function ( dataServer, namespace, options = {} )
 			});
 		}
 
+		/**
+		 * @brief	Always creates a new namespace
+		 *
+		 * @details	Will remove it if it already exists
+		 *
+		 * @return	Promise
+		 */
 		static createNamespace()
 		{
 			return new Promise(( resolve, reject )=>{
@@ -76,6 +127,15 @@ module.exports	= function ( dataServer, namespace, options = {} )
 			});
 		}
 
+		/**
+		 * @brief	Finds one entry in the data server
+		 *
+		 * @details	The promise will resolve to either a ModelClass or null if nothing is found
+		 *
+		 * @param	String recordName
+		 *
+		 * @return	Promise
+		 */
 		static find( recordName )
 		{
 			return new Promise( ( resolve, reject )=>{
