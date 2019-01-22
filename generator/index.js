e90ddf245d0bb14b60c9c1a91025edea62db1d75
Stefan Genov <michaelpalacce@gmail.com>
2019-01-23 01:06:08 +0200

    [master] Started working on a project generatpr

---
+++ generator/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Dependencies
+const { Server, Loggur }	= require( 'event_request' );
+
+/**
+ * @brief	Instantiate the server
+ */
+const server	= Server();
+
+server.apply( 'er_env' );
+server.apply( 'er_static_resources', { paths : [process.env.STATIC_PATH, 'favicon.ico'] } );
+server.apply( 'er_timeout', { timeout : process.env.REUQEST_TIMEOUT } );
+// server.apply( 'er_templating_engine', { templateDir : path.join( PROJECT_ROOT, process.env.TEMPLATING_DIR ), engine : templatingEngine } );
+
+server.get( '/', 'GET', ( event )=>{
+	event.send( '<h1>Hello World</h1>' );
+});
+
+// Start the server
+server.start( ()=>{
+	Loggur.log( 'Server started' );
+});
