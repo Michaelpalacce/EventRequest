2507452346fd4b47fa86d61dcc5b0afbda7c633b
michaelpalacce <michaelpalacce@gmail.com>
2019-01-24 22:53:33 +0200

    [master] Found out a way to get the dirs

--- cli.js
+++ cli.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log( __dirname );
+const path	= require( 'path' );
+
+const projectDir	= path.resolve( __dirname, './generator' );
+const clientDir		= process.cwd();
+
+console.log( projectDir );
+console.log( clientDir );
