fdc723ec9ea231afe19190a3b9ba659e45a2b71a
stefan.genov <stefantigro@gmail.com>
2020-08-06 00:10:59 +0300

    [develop] v28.0.0 added an extra test that the session works with response.end too

--- tests/server/server_er_session_test.js
+++ tests/server/server_er_session_test.js
@@ -72,6 +72,73 @@ test({
 });
 
 test({
+	message	: 'Server.test.er_session.works.as.expected.with.response.end',
+	test	: ( done ) => {
+		const name	= 'testErSessionWithEnd';
+
+		assert.throws(() => {
+			const appOne	= new Server();
+			appOne.apply( appOne.er_session );
+		});
+
+		app.apply( app.er_data_server );
+		app.apply( app.er_session );
+
+		app.get( `/${name}`, async ( event ) => {
+			await event.initSession();
+
+			assert.equal( event.session instanceof Session, true );
+			const session	= event.session;
+
+			if ( session.has( 'authenticated' ) === false )
+			{
+				assert.deepStrictEqual( session.get( 'authenticated' ), null );
+				session.add( 'authenticated', true );
+			}
+			else
+			{
+				assert.equal( session.get( 'authenticated' ), true );
+				event.setResponseHeader( 'authenticated', 1 );
+			}
+
+			event.response.end( name );
+		} );
+
+		helpers.sendServerRequest( `/${name}` ).then(( response ) => {
+			assert.equal( response.body.toString(), name );
+			assert.equal( typeof response.headers['set-cookie'] !== 'undefined', true );
+
+			const cookies	= {},
+				rc		= response.headers['set-cookie'][0];
+
+			rc && rc.split( ';' ).forEach( function( cookie ) {
+				const parts						= cookie.split( '=' );
+				cookies[parts.shift().trim()]	= decodeURI( parts.join( '=' ) );
+			});
+
+			assert.equal( typeof cookies.sid === 'string', true );
+
+			const headers	= { cookie: `sid=${cookies.sid}`};
+
+			return helpers.sendServerRequest( `/${name}`, 'GET', 200, '', headers );
+		}).then(( response ) => {
+			assert.equal( response.body.toString(), name );
+			assert.equal( typeof response.headers.authenticated !== 'undefined', true );
+			assert.equal( response.headers.authenticated, 1 );
+
+			const headers	= { cookie: `sid=wrong`};
+
+			return helpers.sendServerRequest( `/${name}`, 'GET', 200, '', headers );
+		}).then(( response ) => {
+			assert.equal( response.body.toString(), name );
+			assert.equal( typeof response.headers.authenticated === 'undefined', true );
+
+			done();
+		}).catch( done );
+	}
+});
+
+test({
 	message	: 'Server.test.er_session.works.as.expected.with.map',
 	test	: ( done ) => {
 		const app	= new Server();
